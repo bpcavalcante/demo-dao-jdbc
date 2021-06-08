0edbf9f9834f82eb4408800e9326ddcbeefe100f
Bruno <69259703+bpcavalcante@users.noreply.github.com>
2021-06-07 22:05:49 -0300

    SellerDAOJDBC e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,19 +2,23 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDAO;
 import model.entites.Department;
 import model.entites.Seller;
 
 public class Program {
-	
+
 	public static void main(String[] args) {
-		
+
 		Department obj = new Department(1, "TI");
-		
+
 		Seller seller = new Seller(21, "Bruno", "bruno@gmail.com", new Date(), 3000.0, obj);
-		
+
+		SellerDAO sellerDao = DaoFactory.createSellerDao();
+
 		System.out.println(seller);
-		
+
 	}
 
 }
--- src/model/dao/SellerDAO.java
+++ src/model/dao/SellerDAO.java
@@ -1,5 +1,24 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entites.Seller;
+
 public interface SellerDAO {
 
+	void insert(Seller obj);
+
+	void update(Seller onj);
+
+	void deleteById(Integer id);
+
+	// Será responsavel por pegar o id e consultar no banco de dados um objeto com
+	// este id
+	// Se existir vai retornar se não existir vai retonar nulo
+	Seller findById(Integer id);
+
+	// Será responsavel por trazer todos vendedores
+	List<Seller> findAll();
+
+	
 }
